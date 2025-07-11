ac6eb2a2aee234ae5c60f704f0c53af15d2d2e74
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2025-07-12 02:19:55 +0530

    Add files via upload

---
+++ LC_04_Literals.java
@@ -0,0 +1,26 @@
+public class LC_04_Literals {
+    public static void main (String[] args) {
+
+        // integer literal 
+        int  a = 10;  // decimal
+
+        char b = 'A'; // character literal
+
+        float c = 10.5f; // floating pont literal
+
+        double d = 20.55; // double literal
+
+        boolean e = true; // boolean literal
+
+        String f = "Hello World"; // String literal
+
+        System.out.println ("The integer value is: " +a);
+        System.out.println ("The character value is: " +b);
+        System.out.println ("The float value is: " +c);
+        System.out.println ("The double value is: " +d);
+        System.out.println ("The boolean value is: " +e);
+        System.out.println ("The String value is: " +f);
+        
+
+    }
+}
