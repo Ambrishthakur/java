e3826727f179d95aa7988c37d907f1bced2da8fc
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2025-07-12 02:19:35 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+// public class Addition {
+//     public static void main(String[] args) {
+
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+//program for addition of two numbers
+
+public class Addition {
+
+    public static void main (String[] aegs) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println ("The sum of " +a+ " and " +b+ " is " +c);
+    }
+}
