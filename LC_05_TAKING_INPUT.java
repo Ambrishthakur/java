94c383a5051e231086b3986fccdb1a8cb8c70ff5
Ambrishthakur <114800245+Ambrishthakur@users.noreply.github.com>
2025-07-13 13:41:23 +0530

    Add files via upload

---
+++ LC_05_TAKING_INPUT.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+// this is a simple program to take input from the user 
+
+
+// public class LC_05_TAKING_INPUT {
+//     public static void main (String[] args) {
+
+
+//         System.out.println("Taking input from user");
+//         Scanner sc = new Scanner (System.in);
+
+//         System.out.println("Enter first number:");
+//         int a = sc.nextInt(); // taking integer input
+
+//         System.out.println("Enter second number:");
+//         int b = sc.nextInt();
+
+//         int sum = a+b;
+//         System.out.println("sum of these number is ");
+//         System.out.println(sum);
+
+//     }
+// }
+
+
+
+
+// public class LC_05_TAKING_INPUT {
+//     public static void main (String[] args) {
+
+
+//         System.out.println("Taking input from user");
+//         Scanner sc = new Scanner (System.in);
+
+//         System.out.println("Enter first number:");
+//         float a = sc.nextFloat(); // taking integer input
+
+//         System.out.println("Enter second number:");
+//         float b = sc.nextFloat();
+
+//         float sum = a+b;
+//         System.out.println("sum of these number is ");
+//         System.out.println(sum);
+
+//     }
+// }
+
+
+
+public class LC_05_TAKING_INPUT {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //String str = sc.next();
+
+        String str = sc.nextLine();
+
+        System.out.println(str);
+
+        sc.close();
+
+    
+
+
+
+
+    }
+}
