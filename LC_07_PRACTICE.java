cdb43590fe04ce38ab29d1861b348e982e4a802d
Ambrish Thakur <114800245+Ambrishthakur@users.noreply.github.com>
2025-07-14 23:56:03 +0530

    Add files via upload

---
+++ LC_07_PRACTICE.java
@@ -0,0 +1,35 @@
+
+
+public class LC_07_PRACTICE {
+
+
+
+    public static void main(String[] args) {
+
+      // Question1
+
+      //Write a program to sum three numbers in Java.
+
+       int a = 4;
+       int b = 17;
+       int c =6;
+       int sum = a + b+c;
+       System.out.println(sum);
+
+     //  Question2
+
+     //Write a program to calculate CGPA using marks of three subjects (out of 100)
+
+        float subject1 = 45;
+        float subject2 = 95;
+        float subject3 = 48;
+        float cgpa = (subject1 + subject2 +subject3)/30;
+        System.out.println(cgpa);
+
+   
+
+    }
+
+}
+    
+
